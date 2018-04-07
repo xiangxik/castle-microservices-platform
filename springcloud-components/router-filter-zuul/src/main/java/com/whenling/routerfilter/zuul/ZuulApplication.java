f0709c4b218b379c587b7f9f0223cb096d8b3610
kongxiangxi <156801554@qq.com>
2018-04-07 17:44:47 +0800

    config

--- springcloud-components/router-filter-zuul/src/main/java/com/whenling/routerfilter/zuul/ZuulApplication.java
+++ springcloud-components/router-filter-zuul/src/main/java/com/whenling/routerfilter/zuul/ZuulApplication.java
@@ -1,13 +1,11 @@
 package com.whenling.routerfilter.zuul;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableOAuth2Sso
 public class ZuulApplication {
 
 	public static void main(String[] args) {
